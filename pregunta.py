340c12c9dbbd19ac71eaec61970aa6dc992430a4
Carlosdaar <carlosdavidarango78@gmail.com>
2023-06-22 17:30:39 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -2,7 +2,7 @@
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
 
-Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
+Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
@@ -11,10 +11,22 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    #
-    # Inserte su código aquí
-    #
+    # Eliminar filas con valores nulos o vacíos
+    df.dropna(inplace=True)
 
+    # Convertir columnas relevantes a tipos de datos adecuados
+    df['sexo'] = df['sexo'].str.upper()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].astype(str).str.upper()
+    df['idea_negocio'] = df['idea_negocio'].astype(str).str.replace('-', ' ').str.replace('_', ' ').str.upper()
+    df['barrio'] = df['barrio'].str.replace('-', ' ').str.replace('_', ' ').astype(str).str.lower()
+    df['estrato'] = df['estrato'].astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True, errors='coerce')
+    df['monto_del_credito'] = df['monto_del_credito'].replace('[\$,]', '', regex=True).astype(float)
+    df['línea_credito'] = df['línea_credito'].str.replace('-', ' ').str.replace('_', ' ').str.upper()
+
+    # Eliminar filas duplicadas
+    df.drop_duplicates(inplace=True)
     return df
